687e8e900cea96d53e7527162ce23e10295138e0
Akporhoke <akporhokea1@gmailcom>
2026-09-04 23:47:26 +1200

    Portflio to portfolio

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Portfolio = require('../models/Portfolio');
+const Portfolio = require('../models/portfolio');
 const getPrice = require('../utils/getPrice');
 const calculateConfidenceLevel = require('../utils/confidenceCalculator'); // ← ADD THIS
 
